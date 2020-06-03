5fa069d823ed2d22ce509fb32be7bed5db09c67a
Jess220494 <jess.charlton94@gmail.com>
2020-06-02 19:44:59 -0700

    minor changes required to backend

---
+++ backend/api_actiling/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiActilingConfig(AppConfig):
+    name = 'api_actiling'
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_mongodb_docker project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_mongodb_docker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
